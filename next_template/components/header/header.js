08499ebd76af5693e6612cf0b090dc8f7056514c
Fsdexter <fsdexter@gmail.com>
2022-05-25 15:09:50 +0200

    navbar schema ok

--- next_template/components/header/header.js
+++ next_template/components/header/header.js
@@ -1,37 +1,34 @@
 // header react component with home page link, language picker and login button
 import React from "react";
-import Dropdown from "react-bootstrap/Dropdown";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Button from "react-bootstrap/Button";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button'
 import styles from './header.module.css'
 
-export default function Header() {
-    return (
-        <Container>
-            <Row className>
-                <div className={styles.header}r>
-                    <div className={styles.right}>
-                        <a href="/">
-                            Fair car rent
-                        </a>
-                    </div>
-                    <div className={styles.left}>
-                        <Dropdown>
-                            <Dropdown.Toggle variant="success" id="dropdown-basic">
-                                languages
-                            </Dropdown.Toggle>
 
-                            <Dropdown.Menu>
-                                <Dropdown.Item href="#/action-1">English</Dropdown.Item>
-                                <Dropdown.Item href="#/action-2">Español</Dropdown.Item>
-                            </Dropdown.Menu>
-                        </Dropdown>
-                        <Button variant="primary">Login</Button>
-                    </div>
-                </div>
-                
-            </Row>
+export default function Header() {
+  return (
+    <Navbar bg="light" expand="lg"  >
+        <Container fluid>
+            <Navbar.Brand href="#home">Fair car rent</Navbar.Brand>
+            {/* <div className="btn btn-danger float-end">TEST</div>
+            <button className="btn btn-primary m-3" >Button Primary</button> */}
+            <div className={styles.left}>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <NavDropdown title="Languages" id="basic-nav-dropdown" className="">
+                            <NavDropdown.Item href="#action/3.1">Español</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">English</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+                <Button>Log in</Button>
+            </div>
+
         </Container>
-    );
+    </Navbar>
+  );
 }
--- next_template/pages/_app.js
+++ next_template/pages/_app.js
@@ -1,7 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
 export default MyApp
+
+
+
+
